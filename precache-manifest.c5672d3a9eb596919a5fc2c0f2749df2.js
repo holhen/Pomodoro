5323c79b9d26556bcf5dc0ef693e03269df2eea4
holhen <holhen@gmail.com>
2019-08-23 16:31:28 +0200

    Updates

---
+++ precache-manifest.c5672d3a9eb596919a5fc2c0f2749df2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4db3393e192f4ab0ace3f1ced1e3c9c",
+    "url": "/Pomodoro/index.html"
+  },
+  {
+    "revision": "3c866dfbea21291c9af6",
+    "url": "/Pomodoro/static/css/main.7d604f92.chunk.css"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/Pomodoro/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "3c866dfbea21291c9af6",
+    "url": "/Pomodoro/static/js/main.f1f8f47d.chunk.js"
+  },
+  {
+    "revision": "80b30bda6765e0e31afe",
+    "url": "/Pomodoro/static/js/runtime~main.52a691fa.js"
+  },
+  {
+    "revision": "28a7e35e74e4cb36cd2fc410c59f74a0",
+    "url": "/Pomodoro/static/media/beep.28a7e35e.wav"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/Pomodoro/static/media/digital-7.1e670d88.ttf"
+  }
+]);
